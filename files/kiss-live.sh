fe37a51c5212173bfa2a55d6376f7c53d4910b9c
Eudald Gubert i Roldan <hola@eudald.gr>
2020-11-04 11:31:22 +0100

    kiss-live: improvements

---
+++ files/kiss-live.sh
@@ -0,0 +1,11 @@
+export KISS_SU=su
+export KISS_PROMPT=0
+export KISS_HOOK=/root/.cache/hooks
+export KISS_PATH=/var/db/kiss/repo/core
+KISS_PATH=$KISS_PATH:/var/db/kiss/repo/extra
+KISS_PATH=$KISS_PATH:/var/db/kiss/repo/xorg
+KISS_PATH=$KISS_PATH:/var/db/kiss/community/community
+KISS_PATH=$KISS_PATH:/var/db/kiss/live
+export CFLAGS="-march=x86-64 -mtune=generic -pipe -Os"
+export CXXFLAGS="$CFLAGS"
+export MAKEFLAGS="-j4"
